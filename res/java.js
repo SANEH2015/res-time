fa3321b4e92a16eba012a78418afe9a8898e5e92
unknown <mkhizes834@gmail.com>
2023-11-15 09:07:22 +0000

    time tracker

---
+++ res/java.js
@@ -0,0 +1,66 @@
+let work={
+    daily: {
+        current: 5,
+        previous: 7
+      },
+      weekly: {
+        current: 32,
+        previous: 36
+      },
+      monthly: {
+        current: 103,
+        previous: 128
+      } 
+}
+console.log(work); 
+
+let play= {
+  daily: {
+    current: 1,
+    previous: 2
+  },
+  weekly: {
+    current: 10,
+    previous: 8
+  },
+  monthly: {
+    current: 23,
+    previous: 29
+  }
+}
+console.log(play);
+
+let Study={
+      daily: {
+        current: 0,
+        previous: 1
+      },
+      weekly: {
+        current: 4,
+        previous: 7
+      },
+      monthly: {
+        current: 13,
+        previous: 19
+      }
+    }
+    console.log(Study);
+
+    let Exercise={
+      daily: {
+        current: 1,
+        previous: 1
+      },
+      weekly: {
+        current: 4,
+        previous: 5
+      },
+      monthly: {
+        current: 11,
+        previous: 18
+      }
+    }
+    console.log(Exercise)
+     
+
+    
